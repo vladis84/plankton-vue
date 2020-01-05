6bd3371e716d39b5dfa79ecc2336179bf3d1e0e1
Gordienko Vladislav <gordienkovladis@gmail.com>
2020-01-05 10:05:20 +0500

    Модальное окно собственное состояние

---
+++ js/my-dialog.js
@@ -0,0 +1,65 @@
+Vue.component('my-dialog', {
+    template: `
+<b-modal
+  :title="title"
+  v-model="show"
+  @bv::modal::hide="close"
+>
+  <b-form>
+    <b-form-group v-for="(field, key) in fields"
+      :label="field.title"
+      :label-for="field.name"
+      :description="field.description"
+      :key="key"
+    >
+      <b-form-input
+        :id="field.name"
+        type="text"
+        required
+        v-model="field.value"
+      >
+      </b-form-input>
+    </b-form-group>
+  </b-form>
+  <template v-slot:modal-footer>
+    <div class="float-right">
+      <b-button
+        variant="secondary"
+        class="mr-2"
+        v-on:click="reset"
+      >
+        Отмена
+      </b-button>
+      <b-button
+        variant="primary"
+        v-on:click="save"
+      >
+        Сохранить
+      </b-button>
+    </div>
+  </template>
+</b-modal>
+`,
+    props: {
+        show: {
+            type: Boolean,
+            required: true
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+        fields: {
+            type: Array,
+            required: true
+        }
+    },
+    methods: {
+        close() {
+            this.$emit('form-close');
+        },
+        save() {
+            this.$emit('form-close')
+        }
+    }
+});
--- js/offices.js
+++ js/offices.js
@@ -1,10 +1,26 @@
 Vue.component('offices', {
-    template: `<b-table striped hover :items="items" :fields="fields">
-      <template v-slot:cell(action)>
-        <b-btn variant="outline-primary">Изменить</b-btn>
-        <b-btn variant="outline-danger ml-1">Удалить</b-btn>
-      </template>
-    </b-table>`,
+    template: `
+      <div class="card mt-4">
+        <div class="card-header">
+          Таблица офисов для планктончиков
+          <div class="float-right">
+            <b-btn variant="primary" @click="formShow=true">Добавить</b-btn>
+          </div>
+        </div>
+        <b-table striped hover :items="items" :fields="fields">
+          <template v-slot:cell(action)>
+            <b-btn variant="outline-primary">Изменить</b-btn>
+            <b-btn variant="outline-danger ml-1">Удалить</b-btn>
+          </template>
+        </b-table>
+        <my-dialog
+          :show="form.show"
+          title="Добавить офис"
+          :fields="formFields()"
+          @form-close="formShow=false"
+        >
+        </my-dialog>
+      </div>`,
 
     computed: {
         items() {
@@ -19,7 +35,36 @@ Vue.component('offices', {
                 {key: 'name', label: 'Название'},
                 {key: 'address', label: 'Адрес'},
                 {key: 'action', label: 'Управление'},
-            ]
+            ],
+            formShow: false,
         };
+    },
+
+    methods: {
+        save() {
+        },
+        reset() {
+            this.showForm = false;
+        },
+        formFields(values = []) {
+            if (values.length === 0) {
+                values.push('Новый офис', 'Новый адрес');
+            }
+
+            return [
+                {
+                    title: 'Название',
+                    name: 'name',
+                    description: 'Укажите название офиса',
+                    value: values[0]
+                },
+                {
+                    title: 'Адрес',
+                    name: 'address',
+                    description: 'Укажите адрес офиса',
+                    value: values[1]
+                }
+            ]
+        }
     }
 });
